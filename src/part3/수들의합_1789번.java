8b699a641b157facf2a77cbcfd66604f1b8b7ea1
rkdgusdnr99 <112935900+rkdgusdnr99@users.noreply.github.com>
2024-07-12 13:46:24 +0900

    실버_5티어_1789번_수들의합_등차수열합

--- src/part3/수들의합_1789번.java
+++ src/part3/수들의합_1789번.java
@@ -6,15 +6,13 @@ public class 수들의합_1789번 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long S = sc.nextLong();
-        int count = 0;
         int num = 1;
-        while (S > 0) {
-            S -= num++;
-            count++;
+        while (2 * S / num > num + 1) {
+            num++;
         }
-        if (S < 0) {
-            count--;
+        if (2 * S / num < num + 1) {
+            num--;
         }
-        System.out.println(count);
+        System.out.println(num);
     }
 }
