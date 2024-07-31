9de193da18ba6a9b19bc3777ccd6ad6a05ef1d62
rkdgusdnr99 <112935900+rkdgusdnr99@users.noreply.github.com>
2024-07-31 17:25:03 +0900

    골드_5티어_14226번_이모티콘_그냥 큐가 더 좋음

--- src/part4/이모티콘_14226번.java
+++ src/part4/이모티콘_14226번.java
@@ -27,12 +27,12 @@ public class 이모티콘_14226번 {
     }
 
     static int bfs() {
-        Queue<Imo> pq = new LinkedList<>();
-        pq.add(new Imo(1, 0, 0));
+        Queue<Imo> queue = new LinkedList<>();
+        queue.add(new Imo(1, 0, 0));
         visited[1][0] = true;
 
-        while (!pq.isEmpty()) {
-            Imo imo = pq.poll();
+        while (!queue.isEmpty()) {
+            Imo imo = queue.poll();
             int count = imo.count;
             int clipboard = imo.clipboard;
             int time = imo.time;
@@ -42,17 +42,17 @@ public class 이모티콘_14226번 {
             // 클립보드에 있는 모든 이모티콘을 화면에 붙여넣기 한다.
             if (clipboard > 0 && count + clipboard < S*2 && !visited[clipboard+count][clipboard]) {
                 visited[count + clipboard][clipboard] = true;
-                pq.add(new Imo(count + clipboard, clipboard, time + 1));
+                queue.add(new Imo(count + clipboard, clipboard, time + 1));
             }
             // 화면에 있는 이모티콘을 모두 복사해서 클립보드에 저장한다.
             if (count > 0 && count < S*2 && !visited[count][count]) {
                 visited[count][count] = true;
-                pq.add(new Imo(count, count, time + 1));
+                queue.add(new Imo(count, count, time + 1));
             }
             // 화면에 있는 이모티콘 중 하나를 삭제한다.
             if (count > 0 && !visited[count - 1][clipboard]) {
                 visited[count - 1][clipboard] = true;
-                pq.add(new Imo(count - 1, clipboard, time + 1));
+                queue.add(new Imo(count - 1, clipboard, time + 1));
             }
         }
 
