24c28d297220d2021e2fdd59c1db194482d04dea
rkdgusdnr99 <112935900+rkdgusdnr99@users.noreply.github.com>
2024-08-08 10:37:56 +0900

    실버_2티어_1890번_점프_visited 필요 없음

--- src/part5/점프_1890번.java
+++ src/part5/점프_1890번.java
@@ -7,7 +7,6 @@ public class 점프_1890번 {
     static int N;
     static int[][] jump;
     static long[][] dp;
-    static boolean[][] visited;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,8 +21,6 @@ public class 점프_1890번 {
         }
         dp = new long[N][N];
         dp[0][0] = 1;
-        visited = new boolean[N][N];
-        visited[0][0] = true;
 
         routeCount();
 
@@ -33,7 +30,7 @@ public class 점프_1890번 {
     static void routeCount() {
         for (int y = 0; y < N; y++) {
             for (int x = 0; x < N; x++) {
-                if (!visited[y][x]) {
+                if (dp[y][x] == 0) {
                     continue;
                 }
                 if (jump[y][x] == 0) {
@@ -41,11 +38,9 @@ public class 점프_1890번 {
                 }
                 if (y + jump[y][x] < N) {
                     dp[y + jump[y][x]][x] += dp[y][x];
-                    visited[y + jump[y][x]][x] = true;
                 }
                 if (x + jump[y][x] < N) {
                     dp[y][x + jump[y][x]] += dp[y][x];
-                    visited[y][x + jump[y][x]] = true;
                 }
             }
         }
