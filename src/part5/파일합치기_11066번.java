f21d7615d84e8e5a629a49cc0d1c846fe0e13f4b
rkdgusdnr99 <112935900+rkdgusdnr99@users.noreply.github.com>
2024-09-06 22:57:52 +0900

    골드_3티어_파일합치기_11066번_더 효율적

--- src/part5/파일합치기_11066번.java
+++ src/part5/파일합치기_11066번.java
@@ -35,12 +35,12 @@ public class 파일합치기_11066번 {
     static void minCost(int K, int[][][] dp) {
         for (int fileNum = 2; fileNum <= K; fileNum++) { // 파일 묶음의 수
             for (int start = fileNum; start <= K; start++) { // 비교 시작점
+                int sum = dp[1][start-fileNum+1][0] + dp[fileNum-1][start][0]; // 새로 더해질 값
+                dp[fileNum][start][0] = sum;
                 for (int i = 1; i < fileNum; i++) { // i : fileNum-i로 파일 묶음을 나눔
                     int tot = dp[i][start-fileNum+i][1] + dp[fileNum-i][start][1]; // 이전까지 계산한 값
-                    int sum = dp[i][start-fileNum+i][0] + dp[fileNum-i][start][0]; // 새로 더해질 값
                     if (dp[fileNum][start][1] > tot + sum) {
                         dp[fileNum][start][1] = tot + sum;
-                        dp[fileNum][start][0] = sum;
                     }
                 }
             }
