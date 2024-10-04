5cdd96e01b009f7e8502e0033fce201e7e6f90ca
rkdgusdnr99 <112935900+rkdgusdnr99@users.noreply.github.com>
2024-10-04 21:18:06 +0900

    골드_3티어_사회망서비스(SNS)_2533번_주석추가

--- src/part6/사회망서비스_2533번.java
+++ src/part6/사회망서비스_2533번.java
@@ -47,9 +47,9 @@ class Friend {
         friend.friendsNumber++;
     }
 
-    // 두 친구 모두 earlyAdaptor가 아닌 경우에만 한 친구를 earlyAdaptor로 지정
+    // 마지막 노드의 친구가 얼리어답터가 될 수 있는지 판정해서 될 수 있다면 true, 없다면 false 반환
     public boolean becomeEarlyAdopter(Friend friend) {
-        if (!friend.isEarlyAdaptor() && !this.isEarlyAdaptor()) {
+        if (!friend.isEarlyAdaptor() && !this.isEarlyAdaptor()) { // 둘다 얼리어답터가 아니라면 하나는 무조건 얼리아답터
             friend.setEarlyAdaptor();
             return true;
         }
@@ -57,8 +57,8 @@ class Friend {
     }
 
     public Friend removeLastNode() {
-        for(Friend friend : this.getFriendList()) {
-            if (!friend.isVisited()) {
+        for(Friend friend : this.getFriendList()) { // 자신과 연결된 노드들 순회
+            if (!friend.isVisited()) { // 만일 이미 처리된 노드라면 다시 처리해줄 필요 없음
                 friendsNumber--;
                 friend.friendsNumber--;
                 return friend;
@@ -101,7 +101,7 @@ public class 사회망서비스_2533번 {
 
         while (!queue.isEmpty()) {
             Friend friend = queue.poll();
-            friend.setVisited();
+            friend.setVisited(); // 다시 방문하지 않도록 표시
             Friend nextNode = friend.removeLastNode();
             if (nextNode == null) {
                 break;
